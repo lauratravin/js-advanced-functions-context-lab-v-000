09762a62082b479440a75d3da112715d0be6d39c
Laura Messa Travin <laura.travin@gmail.com>
2021-05-05 22:45:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,30 @@
    return employeesArray.map(createEmployeeRecord)
  }
 
+
+ function createTimeInEvent(employeeRecord, dateStamp){
+   let [date, hour] = dateStamp.split(' ')
+
+   employeeRecord.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10), //bcos is a string, should be int
+        date,
+    })
+    return employeeRecord
+ }
+
+
+ function createTimeOutEvent(employeeRecord, dateStamp){
+   let [date, hour] = dateStamp.split(' ')
+
+   employeeRecord.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10), //bcos is a string, should be int
+        date,
+    })
+   return employeeRecord
+
+ }
  //------------------
 
 let allWagesFor = function () {
